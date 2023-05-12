04acc4ec9b788ba9048b9c17ec1d7ea3a78bd0ed
Casper <caspertm@stud.ntnu.no>
2023-05-12 11:26:36 +0200

    #5 setup for runway

--- Airport/Airport/Runway.cs
+++ Airport/Airport/Runway.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace Airport {
     internal class Runway {
+        public int Number;
+        public Queue<string> PlaneQueue;        //contains all planes in queue for take off.
+
+        public Runway(int nr) {
+            Number = nr;
+            PlaneQueue = new Queue<string>();
+
+        }
+
+
+
+
+
+
     }
 }
